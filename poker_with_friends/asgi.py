db07795db344ed7d77c4a72207b5df264ad73ef0
Justin Kelly <justinjkelly2@gmail.com>
2020-07-01 20:05:29 -0700

    idk4

--- poker_with_friends/asgi.py
+++ poker_with_friends/asgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 from channels.layers import get_channel_layer
-from django.core.asgi import get_asgi_application
+#from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poker_with_friends.settings')
 
-application = get_asgi_application()
+#application = get_asgi_application()
 channel_layer = get_channel_layer()
--- poker_with_friends/wsgi.py
+++ poker_with_friends/wsgi.py
@@ -12,7 +12,5 @@ import os
 from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poker_with_friends.settings')
-
-application = get_default_application()
 #application = DjangoWhiteNoise(application)
 
