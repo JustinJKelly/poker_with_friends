0d3c86c71dbac6df5c86ba126975a5e06713f30f
Justin Kelly <justinjkelly2@gmail.com>
2020-07-01 20:49:29 -0700

    idk11

--- poker_with_friends/asgi.py
+++ poker_with_friends/asgi.py
@@ -10,11 +10,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 import os
 #import django
 #from channels.routing import get_default_application
-import channels.asgi
+from channels.layers import get_channel_layer
 #from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poker_with_friends.settings')
 
 #django.setup()
-channel_layer = channels.asgi.get_channel_layer()
+channel_layer = get_channel_layer()
 #application = get_default_application()
