35921240aad6329861bbe7cceb9278ccc3ac131c
Justin Kelly <justinjkelly2@gmail.com>
2020-07-01 20:00:46 -0700

    idk

--- poker_with_friends/wsgi.py
+++ poker_with_friends/wsgi.py
@@ -8,13 +8,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-import django
 #from whitenoise.django import DjangoWhiteNoise
 from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poker_with_friends.settings')
 
-django.setup()
 application = get_default_application()
 #application = DjangoWhiteNoise(application)
 
