838a1313f36e7960ca05973276a7bff1f4993bb9
Justin Kelly <justinjkelly2@gmail.com>
2020-07-01 19:19:10 -0700

    added channel ip

--- poker_with_friends/settings.py
+++ poker_with_friends/settings.py
@@ -79,7 +79,9 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [ ('23.202.231.169',80),
+                       ('127.0.0.1', 6379),
+                      ],
         },
     },
 }
