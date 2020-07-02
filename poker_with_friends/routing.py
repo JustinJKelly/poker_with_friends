6a376184adc54051509655c160c5790e0b93e28b
Justin Kelly <justinjkelly2@gmail.com>
2020-07-01 22:30:02 -0700

    idk28

--- poker_with_friends/routing.py
+++ poker_with_friends/routing.py
@@ -1,5 +1,6 @@
 # mysite/routing.py
 from channels.auth import AuthMiddlewareStack
+from django.contrib.auth.middleware import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import poker.routing
 
