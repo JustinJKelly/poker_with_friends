2de918e7f3a2279bdfacef1eff46d1ee48ccc7f3
Justin Kelly <justinjkelly2@gmail.com>
2020-07-02 21:16:00 -0700

    got it, change debug5

--- poker_with_friends/settings.py
+++ poker_with_friends/settings.py
@@ -144,4 +144,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
