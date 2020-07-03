e0552ef8ec48412692d74466ea7e54811d88496e
Justin Kelly <justinjkelly2@gmail.com>
2020-07-02 19:35:38 -0700

    trying again10

--- poker_with_friends/settings.py
+++ poker_with_friends/settings.py
@@ -34,15 +34,17 @@ django.setup()
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.contenttypes',
     'poker.apps.PokerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'channels',
 ]
+
+django.setup()
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
