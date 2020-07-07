a910a81b788121ad34c132beaf97188bc6869402
Justin Kelly <justinjkelly2@gmail.com>
2020-07-06 17:36:03 -0700

    configured to redirect to https3

--- poker/routing.py
+++ poker/routing.py
@@ -4,10 +4,10 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/(?P<username>\w+)/$', consumers.ChatConsumer),
-    re_path(r'ws/poker/(?P<room_name>\w+)/newUserConnected/(?P<username>\w+)/$', consumers.NewPlayerConsumer),
-    re_path(r'ws/poker/(?P<room_name>\w+)/playerDecision/(?P<username>\w+)/$', consumers.PlayerDecisionConsumer),
-    re_path(r'ws/poker/(?P<room_name>\w+)/getHandWinner/$', consumers.CheckHandWinnerConsumer),
-    re_path(r'ws/poker/(?P<room_name>\w+)/fold/$', consumers.PlayerFoldedConsumer),
-    re_path(r'ws/poker/(?P<room_name>\w+)/dealNewHand/$', consumers.DealNewHandConsumer),
+    re_path(r'wss/chat/(?P<room_name>\w+)/(?P<username>\w+)/$', consumers.ChatConsumer),
+    re_path(r'wss/poker/(?P<room_name>\w+)/newUserConnected/(?P<username>\w+)/$', consumers.NewPlayerConsumer),
+    re_path(r'wss/poker/(?P<room_name>\w+)/playerDecision/(?P<username>\w+)/$', consumers.PlayerDecisionConsumer),
+    re_path(r'wss/poker/(?P<room_name>\w+)/getHandWinner/$', consumers.CheckHandWinnerConsumer),
+    re_path(r'wss/poker/(?P<room_name>\w+)/fold/$', consumers.PlayerFoldedConsumer),
+    re_path(r'wss/poker/(?P<room_name>\w+)/dealNewHand/$', consumers.DealNewHandConsumer),
 ]
