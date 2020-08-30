d1de9fdd51e30a3d4cb60d59e01ded3322009c3e
Justin Kelly <justinjkelly2@gmail.com>
2020-08-30 12:00:32 -0700

    changed asgi

--- poker_with_friends/asgi.py
+++ poker_with_friends/asgi.py
@@ -1,12 +1,20 @@
 """
 ASGI config for poker_with_friends project.
-
 It exposes the ASGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
+#localhost
+'''import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poker_with_friends.settings')
+
+application = get_asgi_application()'''
+
+#prodcution
 import os
 import django
 from channels.routing import get_default_application
