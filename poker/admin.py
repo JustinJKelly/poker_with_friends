9a85c2c5e99c5e9ceac25d821d4db87ef780f782
Justin Kelly <justinjkelly2@gmail.com>
2020-08-31 08:47:09 -0700

    added admin

--- poker/admin.py
+++ poker/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import  Table, Player
+from .models import  Table, Player, SavedTable
 from channels_presence.models import Room
 # Register your models here.
 
 admin.site.register(Table)
 admin.site.register(Player)
 admin.site.register(Room)
+admin.site.register(SavedTable)
